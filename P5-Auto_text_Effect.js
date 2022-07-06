71d6d198ec34727ab3a9b58c9134da629427b5be
Holmes629 <98832423+Holmes629@users.noreply.github.com>
2022-07-06 09:25:53 +0530

    Add files via upload

---
+++ P5-Auto_text_Effect.js
@@ -0,0 +1,26 @@
+str = document.getElementById("index-input");
+i=0;
+time =300;
+sec1 = document.getElementById("sectionid1");
+sec2= document.getElementById("sectionid2");
+timeinput= document.getElementById("input");
+div = document.getElementById("output");
+
+function textgener() {
+    div.innerHTML= str.value.slice(0,i+1)+"|";
+    i++;
+    if (maxlen == i){
+            i=0;
+    }
+    var timer= setTimeout(textgener,time);
+}
+timeinput.addEventListener("input",(e)=> time= 300/e.target.value );
+
+
+function click1(){
+    sec1.className= "section";
+    sec2.className= "sectionClass";
+    maxlen = str.value.length;
+    textgener();
+}
+
